8f695a8a3632a9a09850e8fe96787e1525e08344
Daniel Manor <danielman1803@gmail.com>
2022-05-02 11:41:24 +0300

    Finished Gatsby Tutorial Part 4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 import * as React from "react";
 import Layout from "../components/layout";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home page">
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
       <p>I'm making this by following the Gatsby tutorial.</p>
     </Layout>
   );
